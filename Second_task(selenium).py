12a9ebedd467540812e763caec60be3031375126
archio1 <50983722+archio1@users.noreply.github.com>
2020-11-19 16:47:04 +0200

    first commit

--- Second_task(selenium).py
+++ Second_task(selenium).py
@@ -1,13 +1,14 @@
 
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 browser = webdriver.Chrome("C:/Users/Пользователь/Downloads/chromedriver_win32/chromedriver.exe")
 browser.get("https://www.google.com.tr/search?q=automation+qa")
 
 html_source = browser.page_source
-for i in range(len(html_source) - 1):
-    if "habr.com" in html_source:
-        value = browser.find_element_by_css_selector("[class = 'yuRUbf']")
+value = browser.find_elements_by_link_text("habr.com")# find_element_by_xpath("//a[contains(text(), 'habr.com')]").click()
+for elem in value:
+    if value in html_source:
         print(value)
     else:
         print('false')
