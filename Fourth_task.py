3e248b5ca8a4efeefcdb7258aed060e786ed7925
archio1 <50983722+archio1@users.noreply.github.com>
2020-12-04 19:05:44 +0200

    first commit

--- Fourth_task.py
+++ Fourth_task.py
@@ -1,2 +1,23 @@
 #2)	Зробити на пітоні код який буде виводити всі рядки лога де буде зустрічатись Android 10 використовуючи regex.
 # Дивись прикріплений лог api.parkingwatcher.com.access.log
+
+import re
+
+with open('Resources/api.parkingwatcher.com.access.log', 'r') as f: # уточнить про открытияе файла?? проверки файла?
+    logfile = set(f.read().splitlines())
+data = []
+
+for line in logfile:
+        result = re.findall(r"^(.+Android 10.+)\"$", line)
+        if result != []:
+            print(result)
+
+
+
+
+#print([int(i) for i in result if int(i) > 999999999])
+
+
+
+
+
